fd6e0696315e77918e99d6164ac35b55074af887
nofar <nofar.alfasi@s.afeka.ac.il>
2019-09-27 18:07:34 +0300

    Update left_menu_and_cart_details.php

--- left_menu_and_cart_details.php
+++ left_menu_and_cart_details.php
@@ -3,39 +3,23 @@
 
     <!--Content wrapper starts-->
     <div class="content_wrapper">
-
         <div id="sidebar">
-
             <div id="sidebar_title">Categories</div>
-
             <ul id="cats">
-
                 <?php getCategories(); ?>
-
                 <ul>
-
                     <div id="sidebar_title">Brands</div>
-
                     <ul id="cats">
-
                         <?php getRooms(); ?>
-
                         <ul>
         </div>
 
         <div id="content_area">
-
             <div id="shopping_cart">
-
-					<span style="float:right; font-size:18px; padding:5px; line-height:40px;">
-
-					Welcome Guest! <b style="color:yellow">Shopping Cart -</b> Total Items: Total Price: <a href="cart.php" style="color:yellow">Go to Cart</a>
-
-
-
-					</span>
+                <span style="float:right; font-size:18px; padding:5px; line-height:40px;">
+                    Welcome Guest! <b style="color:yellow">Shopping Cart -</b> Total Items: Total Price: <a href="cart.php" style="color:yellow">Go to Cart</a>
+                </span>
             </div>
-
         </div>
     </div>
     <!--Content wrapper ends-->
